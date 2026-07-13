bea450de728c54aba14a992b5d5f8c4edbc9d132
Shadman Ahmed <131612198+Shadmannn@users.noreply.github.com>
2026-07-13 18:16:30 +0600

    Update up.sh

--- up.sh
+++ up.sh
@@ -1,3 +1,5 @@
+# Please read README about this script if you dont understand what is the purpose of this bash script
+
 #!/bin/bash
 
 sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt clean
